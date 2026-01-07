cc07ff9580d70b11a93f332b2f7f298d5f7c2878
ebin-cp <ebincp@estrotech.in>
2026-01-07 12:40:03 +0530

    remove mykey from ws-test

--- tests/websocket/ws-tests.js
+++ tests/websocket/ws-tests.js
@@ -12,10 +12,10 @@ export function sendWsMetrics(imei, apiKey) {
         socket.on("open",()=>{
             socket.setInterval(()=>{
                 const time_str = (Date.now() * 1000000).toString();
-                const cellular_log = `cellular,imei=${myKey.imei} rssi=16.56,iccid=\"89919509129637837632\",operator=\"airtel\",band=\"LTE BAND 40\",rat=\"TDD LTE\",plmn=\"40495\",apn=\"iot.com\" ${time_str}`;
-                const volume_log = `volume,imei=${myKey.imei} nodeAddress=\"0x01,0x02,0x03\",mask=\"0x20\",sensorValue=6,volume=100.0 ${time_str}`;
-                const fw_log = `firmware,imei=${myKey.imei} firmware_ver_tx=\"v1.0.0:slm-t\",node_0x01_fw=\"v1.0.0:slm-s\",node_0x02_fw=\"v1.0.0:slm-s\" ${time_str}`;
-                const bat_log = `battery,imei=${myKey.imei} eBatVolt=12.00 ${time_str}`;
+                const cellular_log = `cellular,imei=${imei} rssi=16.56,iccid=\"89919509129637837632\",operator=\"airtel\",band=\"LTE BAND 40\",rat=\"TDD LTE\",plmn=\"40495\",apn=\"iot.com\" ${time_str}`;
+                const volume_log = `volume,imei=${imei} nodeAddress=\"0x01,0x02,0x03\",mask=\"0x20\",sensorValue=6,volume=100.0 ${time_str}`;
+                const fw_log = `firmware,imei=${imei} firmware_ver_tx=\"v1.0.0:slm-t\",node_0x01_fw=\"v1.0.0:slm-s\",node_0x02_fw=\"v1.0.0:slm-s\" ${time_str}`;
+                const bat_log = `battery,imei=${imei} eBatVolt=12.00 ${time_str}`;
                 socket.send([cellular_log, volume_log, fw_log, bat_log].join("\n"));
                 ws_metrics_sent_msgs.add(1);
                 }, ws_msg_interval);
