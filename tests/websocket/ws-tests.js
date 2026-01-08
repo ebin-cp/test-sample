bef8fefdba229c7f51e6f2ddf3c61a62950a9119
ebin-cp <ebincp@estrotech.in>
2026-01-08 12:06:34 +0530

    update ws-test js

--- tests/websocket/ws-tests.js
+++ tests/websocket/ws-tests.js
@@ -14,7 +14,6 @@ export function sendWsMetrics(imei, apiKey) {
             const timer = socket.setInterval(() => {
                 const time_str = (Date.now() * 1000000).toString();
                 
-                // Logs definition inside the interval
                 const cellular_log = `cellular,imei=${imei} rssi=16.56,iccid="89919509129637837632",operator="airtel",band="LTE BAND 40",rat="TDD LTE",plmn="40495",apn="iot.com" ${time_str}`;
                 const volume_log = `volume,imei=${imei} nodeAddress="0x01,0x02,0x03",mask="0x20",sensorValue=6,volume=100.0 ${time_str}`;
                 const fw_log = `firmware,imei=${imei} firmware_ver_tx="v1.0.0:slm-t",node_0x01_fw="v1.0.0:slm-s",node_0x02_fw="v1.0.0:slm-s" ${time_str}`;
@@ -24,9 +23,9 @@ export function sendWsMetrics(imei, apiKey) {
                 ws_metrics_sent_msgs.add(1);
                 
                 count++;
-                // 10 messages kazhinjal connection close cheyyum
                 if (count >= 10) { 
-                    socket.clearInterval(timer);
+                    // CHANGE: Use global clearInterval instead of socket.clearInterval
+                    clearInterval(timer); 
                     socket.close(); 
                     console.log(`[VU ${__VU}] - WS streaming finished and socket closed.`);
                 }
