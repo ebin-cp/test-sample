2fa98fa900a2fcba29f06aa9daf7e910a9c7b750
ebin-cp <ebincp@estrotech.in>
2026-01-08 19:30:48 +0530

    update extract summart portion

--- tests/src/load-main-test.js
+++ tests/src/load-main-test.js
@@ -9,7 +9,7 @@ const registrationCount = new Counter("registrations_total");
 
 export const options = {
     vus: 50, 
-    duration: "2m", 
+    duration: "1m", 
     thresholds: {
         'registrations_total': ["count >= 50"],
         'ws_metrics_sent_msgs': ["count >= 10000"],
