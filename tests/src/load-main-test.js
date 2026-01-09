2cba32c826a2265027f1276313289ceb85aab34f
ebin-cp <ebincp@estrotech.in>
2026-01-09 09:49:45 +0530

    add device create to load main test

--- tests/src/load-main-test.js
+++ tests/src/load-main-test.js
@@ -1,11 +1,9 @@
 import http from "k6/http";
 import { check, fail, sleep } from "k6";
 import { Counter } from "k6/metrics";
-// import * as nanoid from "https://esm.run/nanoid";
 import { createDevices } from "../api/device-create.js"; 
 import { sendLoadMetrics } from "../websocket/ws-load.js";
 
-// const gen_imei = nanoid.customAlphabet("1234567890", 15);
 const registrationCount = new Counter("registrations_total");
 
 export const options = {
@@ -17,35 +15,10 @@ export const options = {
     },
 };
 
-// export function setup() {
-//     const deviceKeys = [];
-//     for (let i = 0; i < 50; i++) {
-//         const imei = gen_imei();
-//         const payload = JSON.stringify({ imei: imei });
-//         const params = { headers: { "Content-Type": "application/json" } };
-
-//         const res = http.post(
-//             "http://localhost:8883/api/v1/device",
-//             payload,
-//             params,
-//         );
-//         if (res.status !== 200 || !res.body) {
-//             fail(`Aborting test: Device creation failed for IMEI ${imei}`);
-//         }
-
-//         let d = res.json();
-//         if (!d.key) {
-//             fail(`Aborting test: No key in response for IMEI ${imei}`);
-//         }
-//         registrationCount.add(1);
-//         deviceKeys.push({ api_key: d.key, imei: imei });
-//     }
-//     return { keys: deviceKeys };
-// }
-
 export function setup() {
     const startTimeNS = Date.now() * 1000000;
     const keys = createDevices(50);
+    registrationCount.add(keys.length);
     return { keys: keys, startTimeNS: startTimeNS };
 }
 
