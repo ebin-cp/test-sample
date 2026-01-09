5036e11525ba5c6486bb9c92c478744c6b0431bf
ebin-cp <ebincp@estrotech.in>
2026-01-09 11:51:20 +0530

    update sleep time after assign check

--- tests/src/api-main-test.js
+++ tests/src/api-main-test.js
@@ -30,7 +30,7 @@ export default function(data) {
     // 1. Assign
     const assignRes = assignTruck(myKey.imei, myKey.api_key, truckPayload);
     check(assignRes, {"Assign Status 200":(r) => r.status === 200});
-    sleep(25)
+    sleep(1)
 
     //volume-mapping
     console.log(`[VU ${__VU}] - Verifying Volume Mapping via WS SYNC...`);
