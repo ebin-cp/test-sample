ef1b282dfccc6f7d57b4912914f39039b5e160b6
ebin-cp <ebincp@estrotech.in>
2026-01-31 12:09:57 +0530

    refactor:device-300-1000ms-2min

--- tests/api/device-create.js
+++ tests/api/device-create.js
@@ -1,6 +1,6 @@
 import http from "k6/http";
 
-export function createDevices(count = 190) {
+export function createDevices(count = 300) {
     const deviceKeys = [];
     const generateIMEI = () => Array.from({length: 15}, () => Math.floor(Math.random() * 10)).join("");
 
--- tests/src/api-main-test.js
+++ tests/src/api-main-test.js
@@ -9,7 +9,7 @@ export const options = {
     scenarios: {
         my_test: {
             executor: 'per-vu-iterations',
-            vus: 190,
+            vus: 300,
             iterations: 1,
             maxDuration: '2m', 
         },
@@ -18,7 +18,7 @@ export const options = {
 
 export function setup() {
     const startTimeNS = Date.now() * 1000000;
-    const keys = createDevices(190);
+    const keys = createDevices(300);
     return { keys: keys, startTimeNS: startTimeNS };
 }
 
