7e0285a36836b56f12fcdf8fe1bd354a82d30efd
ebin-cp <ebincp@estrotech.in>
2026-01-31 12:23:22 +0530

    refactor:device-390-1000ms-2min

--- tests/src/api-main-test.js
+++ tests/src/api-main-test.js
@@ -9,7 +9,7 @@ export const options = {
     scenarios: {
         my_test: {
             executor: 'per-vu-iterations',
-            vus: 400,
+            vus: 390,
             iterations: 1,
             maxDuration: '2m', 
         },
@@ -18,7 +18,7 @@ export const options = {
 
 export function setup() {
     const startTimeNS = Date.now() * 1000000;
-    const keys = createDevices(400);
+    const keys = createDevices(390);
     return { keys: keys, startTimeNS: startTimeNS };
 }
 
